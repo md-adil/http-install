6b8cee96c2290e95c24474705d8eceb8791ff815
Md Aqil <aqil@wipl.co.in>
2020-04-07 20:36:16 +0530

    remove unneccessary semicoloumn

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from user_input import site_name
 from server import restart
 
 def run():
-	site = site_name();
+	site = site_name()
 	print("Generating configuration file")
 	conf = generate_host_config(site)
 	print("Writing configuration file")
 	written = write_config(site, conf)
 	print(f"Configuration written to {written} successfully")
 	print("Creating directory")
-	create_site_dir(site);
-	print("Restarting server:");
+	create_site_dir(site)
+	print("Restarting server:")
 	restart()
 	print("Server is restarted.")
 	
